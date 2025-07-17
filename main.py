aeb5f622e5c057622e2c2fea51e6aa82347d69d7
Sameer pareek <sameerpareek500@gmail.com>
2025-07-17 14:19:45 +0530

    Questions added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hi")
+questions = [
+    ["Who is Shah Rukh Khan?", "WWE Wrestler", "Plumber", "Actor", "Astronaut", 3],
+    ["What is the capital of France?", "Berlin", "Paris", "Rome", "London", 2],
+    ["Which planet is known as the Red Planet?", "Earth", "Venus", "Mars", "Jupiter", 3],
+    ["What is the largest mammal?", "Shark", "Blue Whale", "Elephant", "Giraffe", 2],
+    ["Who wrote 'Romeo and Juliet'?", "William Shakespeare", "Jane Austen", "Charles Dickens", "Homer", 1],
+    ["What is the square root of 64?", "8", "10", "6", "12", 1],
+    ["Which country is known as the Land of the Rising Sun?", "India", "South Korea", "Japan", "China", 3],
+    ["Who painted the Mona Lisa?", "Claude Monet", "Pablo Picasso", "Leonardo da Vinci", "Vincent van Gogh", 3],
+    ["What is the fastest land animal?", "Horse", "Lion", "Cheetah", "Elephant", 3],
+    ["Which ocean is the largest?", "Indian Ocean", "Pacific Ocean", "Atlantic Ocean", "Arctic Ocean", 2],
+    ["What is the smallest country in the world?", "San Marino", "Vatican City", "Monaco", "Liechtenstein", 2]
+]
